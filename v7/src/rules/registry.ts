283535b6465fe5d9d91c079d9e2f3873a81e757c
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2025-11-14 12:27:29 +0100

    fix: register all implemented rules

--- v7/src/rules/registry.ts
+++ v7/src/rules/registry.ts
@@ -5,6 +5,7 @@ import { httpStatusRule } from './http/status'
 import { robotsTxtRule } from './robots/robotsTxt'
 import { robotsMetaRule } from './head/robotsMeta'
 import { hreflangRule } from './head/hreflang'
+import { hreflangMultipageRule } from './head/hreflangMultipage'
 import { ogTitleRule } from './og/title'
 import { ogDescriptionRule } from './og/description'
 import { ogUrlRule } from './og/url'
@@ -31,10 +32,13 @@ import { hasHeaderRule } from './http/hasHeader'
 import { soft404Rule } from './http/soft404'
 import { canonicalRedirectsRule } from './head/canonicalRedirects'
 import { h1Rule } from './body/h1'
+import { imagesLayoutRule } from './body/imagesLayout'
+import { imagesLazyRule } from './body/imagesLazy'
 import { nofollowRule } from './body/nofollow'
 import { linkedImagesAltRule } from './a11y/linkedImagesAlt'
 import { internalLinksRule } from './body/internalLinks'
 import { parameterizedLinksRule } from './body/parameterizedLinks'
+import { unsecureInputRule } from './body/unsecureInput'
 import { relAlternateMediaRule } from './head/relAlternateMedia'
 import { ampCacheUrlRule } from './google/ampCacheUrl'
 import { robotsBlockedResourcesRule } from './robots/blockedResources'
@@ -65,6 +69,7 @@ import { gscPageWorldwideRule } from './google/gsc/pageWorldwideSearchAnalytics'
 import { gscDirectoryWorldwideRule } from './google/gsc/pageDirectoryWorldwideSearchAnalytics'
 import { historyStateUpdateRule } from './url/historyStateUpdate'
 import { unavailableAfterRule } from './http/unavailableAfter'
+import { commonMobileSetupRule } from './http/commonMobileSetup'
 import { cacheDeliveryRule } from './http/cacheDelivery'
 import { securityHeadersRule } from './http/securityHeaders'
 import { httpsSchemeRule } from './http/httpsScheme'
@@ -75,6 +80,15 @@ import { topWordsRule } from './dom/topWords'
 import { canonicalChainRule } from './head/canonicalChain'
 import { schemaArticlePresentRule } from './schema/articlePresent'
 import { schemaArticleRequiredRule } from './schema/articleRequired'
+import { schemaBreadcrumbRule } from './schema/breadcrumb'
+import { schemaEventRule } from './schema/event'
+import { schemaFaqRule } from './schema/faq'
+import { schemaHowToRule } from './schema/howto'
+import { schemaJobPostingRule } from './schema/jobPosting'
+import { schemaOrganizationRule } from './schema/organization'
+import { schemaProductRule } from './schema/product'
+import { schemaRecipeRule } from './schema/recipe'
+import { schemaVideoRule } from './schema/video'
 import { discoverMaxImagePreviewLargeRule } from './discover/maxImagePreviewLarge'
 import { discoverTwitterLargeCardRule } from './discover/twitterLargeCard'
 import { discoverArticleStructuredDataRule } from './discover/articleStructuredData'
@@ -96,6 +110,7 @@ export const registry: Rule[] = [
   robotsTxtRule,
   robotsMetaRule,
   hreflangRule,
+  hreflangMultipageRule,
   ogTitleRule,
   ogDescriptionRule,
   ogUrlRule,
@@ -122,10 +137,13 @@ export const registry: Rule[] = [
   soft404Rule,
   canonicalRedirectsRule,
   h1Rule,
+  imagesLayoutRule,
+  imagesLazyRule,
   nofollowRule,
   linkedImagesAltRule,
   internalLinksRule,
   parameterizedLinksRule,
+  unsecureInputRule,
   relAlternateMediaRule,
   ampCacheUrlRule,
   robotsBlockedResourcesRule,
@@ -141,6 +159,7 @@ export const registry: Rule[] = [
   googleIsConnectedRule,
   historyStateUpdateRule,
   unavailableAfterRule,
+  commonMobileSetupRule,
   httpsSchemeRule,
   http2AdvertisedRule,
   http3AdvertisedRule,
@@ -176,4 +195,13 @@ export const registry: Rule[] = [
   discoverPrimaryLanguageRule,
   schemaArticlePresentRule,
   schemaArticleRequiredRule,
+  schemaBreadcrumbRule,
+  schemaEventRule,
+  schemaFaqRule,
+  schemaHowToRule,
+  schemaJobPostingRule,
+  schemaOrganizationRule,
+  schemaProductRule,
+  schemaRecipeRule,
+  schemaVideoRule,
 ]
